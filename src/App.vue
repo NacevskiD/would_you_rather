16931ffd734bf9fe7dc2a07d2358b96de2151a33
David <nacevskidavid@gmail.com>
2021-04-11 23:57:42 -0500

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <h1>Would you rather...</h1>
+
+  <would-you-rather
+  v-bind:questions="questions"
+  v-on:answer-changed="answerChanged"
+  v-on:answer-changed1="answerChanged1"
+  v-on:answer-changed2="answerChanged2">
+  </would-you-rather>
+
+  <p v-for="message in answers">{{message}}</p>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data(){
+    return{
+      answers:[],
+      questions:[
+        {
+          id:0,
+          wyrQ: 'Would you rather be a detective or a pilot?',
+          wyrA1: 'Detective',
+          wyrA2: 'Pilot',
+        },
+        {
+          id:1,
+          wyrQ: 'Would you rather be a wizard or a superhero?',
+          wyrA1: 'Wizard',
+          wyrA2: 'Superhero',
+        },
+        {
+          id:2,
+          wyrQ: 'Would you rather play hide and seek or dodgeball?',
+          wyrA1: 'Hide and seek',
+          wyrA2: 'Dodgeball',
+          }
+      ],
+
+    }
+  },
+  methods:{
+    answerChanged(choice){
+
+      this.answers.push(choice)
+
+    },
+    answerChanged1(choice){
+      this.answers.push(choice)
+
+    },
+    answerChanged2(choice){
+      this.answers.push(choice)
+
+    }
   }
 }
 </script>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+
+    <h2> Please make your choice!</h2>
+
+    <h3>{{questions[0].wyrQ}}</h3>
+
+    <input type="radio" v-model="choice1" v-bind:value="questions[0].wyrA1" v-on:change="choiceMade"><label>{{questions[0].wyrA1}}</label>
+    <input type="radio" v-model="choice1" v-bind:value="questions[0].wyrA2" v-on:change="choiceMade"><label>{{questions[0].wyrA2}}</label>
+
+    <h3>{{questions[1].wyrQ}}</h3>
+
+    <input type="radio" v-model="choice2" v-bind:value="questions[1].wyrA1" v-on:change="choiceMade1"><label>{{questions[1].wyrA1}}</label>
+    <input type="radio" v-model="choice2" v-bind:value="questions[1].wyrA2" v-on:change="choiceMade1"><label>{{questions[1].wyrA2}}</label>
+
+    <h3>{{questions[2].wyrQ}}</h3>
+
+    <input type="radio" v-model="choice3" v-bind:value="questions[2].wyrA1" v-on:change="choiceMade2"><label>{{questions[2].wyrA1}}</label>
+    <input type="radio" v-model="choice3" v-bind:value="questions[2].wyrA2" v-on:change="choiceMade2"><label>{{questions[2].wyrA2}}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    questions: Array,
+
+  },
+  data(){
+    return{
+      choice1:'',
+      choice2:'',
+      choice3:'',
+    }
+  },
+  methods:{
+    choiceMade(){
+      this.$emit('answer-changed', this.choice1)
+    },
+    choiceMade1(){
+      this.$emit('answer-changed1', this.choice2)
+    },
+    choiceMade2(){
+      this.$emit('answer-changed2', this.choice3)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
